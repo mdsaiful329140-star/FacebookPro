504cd1d15695db03b6b34492505fe05224a51b36
mdsaiful329140-star <mdsaiful329140@gmail.com>
2026-06-28 16:25:24 +0600

    Add NextAuth configuration with Google and Email providers

---
+++ prisma/lib/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../lib/prisma";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "database" },
+  pages: {
+    signIn: '/auth/signin'
+  }
+});
